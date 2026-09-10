51c51b798f0c848b90fa0d0cf7673c7e939dd255
zero <pira8@proton.me>
2026-09-09 19:10:37 -0500

    add organization by date

--- main.py
+++ main.py
@@ -2,29 +2,56 @@
 
 import os
 import shutil
+from datetime import datetime
 
-def organize(directory, organize_by_extension):
-    # organize by either name or extension depending whether organize_by_extension is True or False
-    if organize_by_extension:
-        for filename in os.listdir(directory):
-            if os.path.isfile(os.path.join(directory, filename)):
-                extension = os.path.splitext(filename)[1][1:]  # get the file extension without the dot
-                if extension:  # check if the file has an extension
+def organize(directory, organize_by_extension, organize_by_date):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+
+        if os.path.isfile(filepath):
+
+            if organize_by_date:
+                file_date = get_file_date(filepath)
+
+                date_folder = os.path.join(directory, file_date)
+
+                if not os.path.exists(date_folder):
+                    os.makedirs(date_folder)
+
+                shutil.move(filepath, os.path.join(date_folder, filename))
+
+            elif organize_by_extension:
+                extension = os.path.splitext(filename)[1][1:]
+
+                if extension:
                     extension_folder = os.path.join(directory, extension)
+
                     if not os.path.exists(extension_folder):
                         os.makedirs(extension_folder)
-                    shutil.move(os.path.join(directory, filename), os.path.join(extension_folder, filename))
-    else:
-        for filename in os.listdir(directory):
-            if os.path.isfile(os.path.join(directory, filename)):
-                first_letter = filename[0].upper()  # get the first letter of the filename and convert to uppercase
+
+                    shutil.move(filepath, os.path.join(extension_folder, filename))
+
+            else:
+                first_letter = filename[0].upper()
+
                 letter_folder = os.path.join(directory, first_letter)
+
                 if not os.path.exists(letter_folder):
                     os.makedirs(letter_folder)
-                shutil.move(os.path.join(directory, filename), os.path.join(letter_folder, filename))
+
+                shutil.move(filepath, os.path.join(letter_folder, filename))
+
+def get_file_date(filepath):
+    timestamp = os.path.getmtime(filepath)
+    date = datetime.fromtimestamp(timestamp)
+    return date.strftime("%-m-%-d-%y")
 
 def input_directory():
     return input("enter the directory path to organize: ")
+
+def input_organize_by_date():
+    choice = input("Do you wany to organize by date? (y/n): ")
+    return choice.lower() == 'y'
 
 def input_organize_by_extension():
     choice = input("Do you want to organize by file extension? (y/n): ")
@@ -33,11 +60,21 @@ def input_organize_by_extension():
 def main():
     directory = input_directory()
     organize_by_extension = input_organize_by_extension()
-    if os.path.exists(directory) and os.path.isdir(directory):
-        organize(directory, organize_by_extension)
-        print("files have been organized.")
+    organize_by_date = 'n'
+    if organize_by_extension == 'y':
+
+        if os.path.exists(directory) and os.path.isdir(directory):
+            organize(directory, organize_by_extension, organize_by_date)
+            print("files have been organized.")
+        else:
+            print("invalid directory path.")
     else:
-        print("invalid directory path.")
+        organize_by_date = input_organize_by_date()
+        if os.path.exists(directory) and os.path.isdir(directory):
+            organize(directory, organize_by_extension, organize_by_date)
+            print("files have been organized.")
+        else:
+            print("invalid directory path.")
 
 if __name__ == "__main__":
     main()
